a55991ecff9e84b29ce808cd12cb87d7159e09b7
mmehedim <121457696+mmehedim@users.noreply.github.com>
2026-07-18 15:26:38 -0400

    Change 'released' to 'Last Modified' in script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const software = [
     Name:"📂 E-BOT Recovery",
     version:"v1.0.0",
     size:"152 MB",
-    released:"18 Jul 2026",
+    Last Modified:"18 Jul 2026",
     download:"https://github.com/USERNAME/e-bot-download-page/releases/download/v2.1.0/E-BOT-Recovery.exe"
 },
 
@@ -12,7 +12,7 @@ const software = [
     Name:"📂 E-BOT Recovery",
     version:"v1.0.0",
     size:"149 MB",
-    released:"16 Jul 2026",
+    Last Modified:"16 Jul 2026",
     download:"https://github.com/USERNAME/e-bot-download-page/releases/download/v1.5.2/Account-Creator.exe"
 },
 
@@ -20,7 +20,7 @@ const software = [
     Name:"📂 E-BOT Recovery",
     version:"v1.0.0",
     size:"138 MB",
-    released:"10 Jul 2026",
+    Last Modified:"10 Jul 2026",
     download:"https://github.com/USERNAME/e-bot-download-page/releases/download/v3.0/Proxy-Manager.exe"
 },
 
@@ -28,7 +28,7 @@ const software = [
     Name:"📂 E-BOT Recovery",
     version:"v1.0.0",
     size:"138 MB",
-    released:"10 Jul 2026",
+    Last Modified:"10 Jul 2026",
     download:"https://github.com/USERNAME/e-bot-download-page/releases/download/v3.0/Proxy-Manager.exe"
 },
 
@@ -37,7 +37,7 @@ const software = [
     Name:"📂 E-BOT Recovery",
     version:"v1.0.0",
     size:"157 MB",
-    released:"03 Jul 2026",
+    Last Modified:"03 Jul 2026",
     download:"https://github.com/USERNAME/e-bot-download-page/releases/download/v4.4/Cookie-Checker.exe"
 }
 
@@ -51,13 +51,13 @@ table.innerHTML += `
 
 <tr>
 
-<td>${item.program}</td>
+<td>${item.Name}</td>
 
 <td>${item.version}</td>
 
 <td>${item.size}</td>
 
-<td>${item.released}</td>
+<td>${item.Last Modified}</td>
 
 <td>
 
