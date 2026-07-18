e5c2a1d81c97916e1695082e7885cdc0755864cb
mmehedim <121457696+mmehedim@users.noreply.github.com>
2026-07-18 14:46:24 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,31 +2,40 @@ const software = [
 
 {
     program:"📂 E-BOT Recovery",
-    version:"2.1.0",
+    version:"v1.0.0",
     size:"152 MB",
     released:"18 Jul 2026",
     download:"https://github.com/USERNAME/e-bot-download-page/releases/download/v2.1.0/E-BOT-Recovery.exe"
 },
 
 {
-    program:"📄 Account Creator",
-    version:"1.5.2",
+    program:"📂 E-BOT Recovery",
+    version:"v1.0.0",
     size:"149 MB",
     released:"16 Jul 2026",
     download:"https://github.com/USERNAME/e-bot-download-page/releases/download/v1.5.2/Account-Creator.exe"
 },
 
 {
-    program:"📁 Proxy Manager",
-    version:"3.0",
+    program:"📂 E-BOT Recovery",
+    version:"v1.0.0",
     size:"138 MB",
     released:"10 Jul 2026",
     download:"https://github.com/USERNAME/e-bot-download-page/releases/download/v3.0/Proxy-Manager.exe"
 },
 
 {
-    program:"Cookie Checker",
-    version:"4.4",
+    program:"📂 E-BOT Recovery",
+    version:"v1.0.0",
+    size:"138 MB",
+    released:"10 Jul 2026",
+    download:"https://github.com/USERNAME/e-bot-download-page/releases/download/v3.0/Proxy-Manager.exe"
+},
+
+    
+{
+    program:"📂 E-BOT Recovery",
+    version:"v1.0.0",
     size:"157 MB",
     released:"03 Jul 2026",
     download:"https://github.com/USERNAME/e-bot-download-page/releases/download/v4.4/Cookie-Checker.exe"
