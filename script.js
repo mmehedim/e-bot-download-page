b94170c7dce9a5c9917375761e9bf6932fe0f05a
mmehedim <121457696+mmehedim@users.noreply.github.com>
2026-07-19 13:01:38 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const software = [
     version:"v1.0.0",
     size:"17 MB",
     released:"19 Jul 2026",
-    download:"your download link"
+    download:"https://github.com/mmehedim/e-bot-download-page/releases/download/E-Bot_Hunter/E-Bot_Hunter.exe"
 },
 
 {
