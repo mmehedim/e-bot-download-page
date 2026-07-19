5951da83ab8575df53f16780a334c5d5a9660e38
mmehedim <121457696+mmehedim@users.noreply.github.com>
2026-07-19 12:35:17 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,59 +1,35 @@
 const software = [
 
 {
-    name:"📂 E-BOT Recovery",
-    version:"2.1.0",
-    size:"152 MB",
-    released:"22 Sep 2025 01:33 PM",
-    download:"https://github.com/USERNAME/e-bot-download-page/releases/download/v2.1.0/E-BOT-Recovery.exe"
-},
-
-{
-    name:"📂 E-BOT Recovery",
-    version:"1.5.2",
-    size:"149 MB",
-    released:"16 Jul 2026",
-    download:"https://github.com/USERNAME/e-bot-download-page/releases/download/v1.5.2/Account-Creator.exe"
-},
-
-{
-    name:"📂 E-BOT Recovery",
-    version:"3.0",
-    size:"138 MB",
-    released:"10 Jul 2026",
-    download:"https://github.com/USERNAME/e-bot-download-page/releases/download/v3.0/Proxy-Manager.exe"
-},
-
-{
-    name:"📂 E_Bot_Match",
+    name:"📂 E-Bot_Hunter.exe",
     version:"v1.0.0",
-    size:"92 MB",
-    released:"13 Jul 2026",
+    size:"17 MB",
+    released:"19 Jul 2026",
     download:"your download link"
 },
 
 {
-    name:"📂 E_bot_N_self",
+    name:"📂 E-Bot_Mail_Recovery.exe",
     version:"v1.0.0",
-    size:"42 MB",
-    released:"22 Sep 2025",
+    size:"29 MB",
+    released:"19 Jul 2026",
     download:"your download link"
 },
 
 {
-    name:"📂 E_Bot_Rec_Pro",
+    name:"📂 E-BOT_Number.exe",
     version:"v1.0.0",
-    size:"113 MB",
-    released:"08 Jul 2026",
+    size:"36 MB",
+    released:"19 Jul 2026",
     download:"your download link"
 },
-    
+
 {
-    name:"📂 E-BOT Recovery",
-    version:"4.4",
-    size:"157 MB",
-    released:"03 Jul 2026",
-    download:"https://github.com/USERNAME/e-bot-download-page/releases/download/v4.4/Cookie-Checker.exe"
+    name:"📂 E-Bot_Rec_Pro.exe",
+    version:"v1.0.0",
+    size:"18 MB",
+    released:"19 Jul 2026",
+    download:"your download link"
 }
 
 ];
